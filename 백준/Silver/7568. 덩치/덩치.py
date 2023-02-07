037b7cd1f76e701d0380a6ad285a2dd0878de882
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-02-07 09:36:49 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+people = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+
+frame_list = []
+for i in range(N):
+    frame = 1
+    for j in range(N):
+        if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+            frame += 1
+    frame_list.append(frame)
+
+print(*frame_list)
