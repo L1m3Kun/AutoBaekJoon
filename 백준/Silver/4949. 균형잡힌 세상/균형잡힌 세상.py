c694d8132c853cf4c164428f74a0d434265a98c2
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-02-14 09:06:45 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,21 +1,25 @@
 import sys
 
 def solution():
-    stack = []
+    stack = []  # stack
+    # 괄호 확인용 리스트
     open_p = ['(', '[']
     close_p = [')', ']']
     for i in sentence:
-        for j in i:
-            if j in open_p:
-                stack.append(j)
-            elif j in close_p:
-                if stack:
-                    if open_p.index(stack[-1]) == close_p.index(j):
-                        stack.pop()
-                    else:
-                        return 'no'
+        # 괄호가 open에 있을 때는 stack에 추가
+        if i in open_p:
+            stack.append(i)
+        # close에 있을 때는 stack에 뭐가 있는지 보고 없으면 no
+        elif i in close_p:
+            if stack:
+                # 있더라도 짝이 안맞으면 no
+                if open_p.index(stack[-1]) == close_p.index(i):
+                    stack.pop()
                 else:
                     return 'no'
+            else:
+                return 'no'
+    # stack에 있는 열린 괄호들이 짝이 다 맞으면 yes
     if len(stack) == 0:
         return 'yes'
     else:
@@ -24,13 +28,8 @@ def solution():
 
 while True:
     sentence = list(sys.stdin.readline())
+    # readline()으로 가져오기때문에 \n 기호가 포함되어있음
     if sentence == ['.', '\n']:
         break
     else:
         print(solution())
-
-
-
-
-
-
