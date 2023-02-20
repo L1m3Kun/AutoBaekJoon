b4280a2b7b4678dd57e08d4f32ddbe830bb9677a
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-02-20 13:15:52 +0900

    [Silver IV] Title: 큐 2, Time: 2376 ms, Memory: 175120 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+que = deque()                            # queue를 deque로 생성
+for _ in range(N):
+    order = deque(sys.stdin.readline().strip().split())
+    
+    if order[0] == 'push':
+        que.append(order[1])
+    elif len(que) == 0:
+        if order[0] == 'pop' or order[0] == 'front' or order[0] == 'back':
+            print(-1)
+        elif order[0] == 'empty':
+            print(1)
+        elif order[0] == 'size':
+            print(len(que))
+    else:
+        if order[0] == 'pop':
+            print(que.popleft())           # 첫번째 인자 빼기
+        elif order[0] == 'size':
+            print(len(que))
+        elif order[0] == 'empty':
+            print(0)
+        elif order[0] == 'front':
+            print(que[0])
+        elif order[0] == 'back':
+            print(que[-1])
+    
