102d642a59cd7254d2f111a946dc96ba1779feab
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-03-10 08:49:46 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+ladder = {}
+for _ in range(N+M):
+    spot, node = map(int, input().strip().split())
+    ladder[spot] = node
+
+queue = deque([1])
+visited = [0,1] + [0] * 99
+while queue:
+    start = queue.popleft()
+    if start == 100:
+        break
+    for go in range(1, 7):
+        idx = start + go
+        if idx in ladder.keys():
+            idx = ladder[idx]
+        if idx <= 100 and not visited[idx]:
+            visited[idx] = visited[start] + 1
+            queue.append(idx)
+print(visited[100]-1)
