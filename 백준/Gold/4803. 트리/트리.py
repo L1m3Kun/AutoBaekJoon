415dc35533fed6ca6481da5a60dfb428d5fe00ba
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-03-12 22:20:49 +0900

    [Gold IV] Title: 트리, Time: 888 ms, Memory: 38000 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -2,40 +2,36 @@ import sys
 # sys.stdin = open('Tree\input.txt', 'r')
 input = sys.stdin.readline
 test_case = 0
-while True:
+while True:     # 입력 받기
     n, m = map(int, input().strip().split())
     test_case += 1
     if n == 0 and m == 0:
         break
     else:
+        # 그래프 만들기
         graph = [[] for _ in range(n+1)]
-        visited = []
+        visited = [0] * (n+1)
         for i in range(m):
             spot, node = map(int, input().strip().split())
             graph[spot].append(node)
             graph[node].append(spot)
-        cnt = 0
+        cnt = 0         # 트리 세기 용
         for i in range(1, n+1):
-            if i not in visited:
-                root = [[] for _ in range(n+1)]
+            if not visited[i]:
+                root = [[] for _ in range(n+1)]     # 트리의 부모를 저장하여 부모가 둘 이상이면 그래프 아니면 트리
                 stack = [i]
-                graph_visited = [0] * (n+1)
-                graph_visited[i] = 1
+                visited[i] = 1
                 while stack:
                     t = stack.pop()
-                    if t not in visited:
-                        visited.append(t)
                     for j in graph[t]:
-                        if j not in root[t]:
+                        if j not in root[t]:        # 부모 정점 저장
                             root[j].append(t)
-                        if not graph_visited[j]:
-                            graph_visited[j] += 1
+                        if not visited[j]:
                             stack.append(j)
+                            visited[j] += 1
                         else:
-                            graph_visited[j] += 1
+                            visited[j] += 1
                 for k in range(1, n+1):
-                    # if graph_visited[k] > 2:
-                    #     break
                     if len(root[k])>1:
                         break
                 else:
