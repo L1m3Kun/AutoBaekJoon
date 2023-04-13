49143d8cb46ab302cb4b41f8356a2a61a52f8dac
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-04-13 23:19:52 +0900

    [Silver V] Title: 집합, Time: 3872 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,40 +2,27 @@
 import sys
 input = sys.stdin.readline
 
-def add(x):
-    S.add(x)
-
-def remove(x):
-    S.discard(x)
-
-def check(x):
-    if x in S:
-        return 1
-    else:
-        return 0
-
-def toggle(x):
-    if x in S:
-        remove(x)
-    else:
-        add(x)
-
-    
-
 M = int(input())
-S = set()
-
+S = 0
 for _ in range(M):
     order= list(input().strip().split())
     if order[0] == 'add':
-        add(int(order[1]))
+        S |= (1<<int(order[1]))
     elif order[0] == 'remove':
-        remove(int(order[1]))
+        S -= S&(1<<int(order[1]))
     elif order[0] == 'check':
-        print(check(int(order[1])))
+        if S & (1<<int(order[1])):
+            print(1)
+        else:
+            print(0)
     elif order[0] == 'toggle':
-        toggle(int(order[1]))
+        if S & (1<<int(order[1])):
+            S &= (0<<int(order[1]))
+        else:
+            S |= (1<<int(order[1]))
     elif order[0] == 'all':
-        S = set(i for i in range(1,21))
-    elif order[0] == 'empty':
-        S = set()
+        # for i in range(1,21):
+        #     S |= (1<<i)
+        S =2097150
+    elif order == 'empty':
+        S = 0
