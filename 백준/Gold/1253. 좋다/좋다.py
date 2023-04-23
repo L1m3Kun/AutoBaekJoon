b7854363894697b5628c8b41a6aeb460526349ea
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-04-23 22:04:47 +0900

    [Gold IV] Title: 좋다, Time: 892 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 
-hash = {}
 arr = list(map(int, input().split()))
 if sum(arr):
+    hash = {}
     for i in range(N):
       hash[arr[i]] = i
     cnt = 0
@@ -13,8 +13,6 @@ if sum(arr):
         for j in range(N):
           add = arr[i] - arr[j]
           if i!= j and add in hash and hash[add] != i and hash[add] != j:
-        
-        # print("good = ", add)
             cnt += 1
             break
     print(cnt)
