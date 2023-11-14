448c8a0da3d7734fdf7d1873c48a7038d1961816
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2023-11-14 09:40:19 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,22 @@
+# 11050 이항 계수 1
+import sys
+
+input = sys.stdin.readline
+
+
+def factorial(N: int) -> list[int]:
+    ft = [0] * (N + 1)
+    ft[0], ft[1] = 1, 1
+    for i in range(2, N + 1):
+        ft[i] = i * ft[i - 1]
+    return ft
+
+
+def solution(N: int, K: int) -> int:
+    f = factorial(N)
+    return f[N] // (f[K] * f[N - K])
+
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    print(solution(N, K))
