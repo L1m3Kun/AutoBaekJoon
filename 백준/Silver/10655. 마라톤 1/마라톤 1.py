ab5662bf2508cb756bc2c0ec08e22d04669097f3
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-05-08 18:12:19 +0900

    [Silver III] Title: 마라톤 1, Time: 224 ms, Memory: 44188 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.py
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.py
@@ -7,20 +7,23 @@ def main():
     N = int(input())
     checkPoints = [tuple(map(int, input().split())) for _ in range(N)]
 
+    # 거리 계산용
     def distance(x1, y1, x2, y2):
         return abs(x1-x2) + abs(y1-y2)
 
-    
-    total = 0
-    maxDiffer = -1000000
+    total = 0       # 스킵 안했을 때 거리
+    maxDiffer = -1000000    # 스킵했을 때와 안 했을 때의 최댓값
     for idx in range(1,N-1):
+        # 인덱싱
         (previ,prevj), (i,j), (posti,postj) = checkPoints[idx-1], checkPoints[idx], checkPoints[idx+1]
+        # 스킵 안 했을 때 거리
         nonSkip = [distance(previ,prevj,i,j), distance(i,j,posti,postj)]
+        # 스킵 했을 때 거리
         skipped = distance(previ,prevj,posti,postj)
-        differ = sum(nonSkip) - skipped
+        maxDiffer = max(sum(nonSkip) - skipped, maxDiffer)
         total += nonSkip[0]
-        maxDiffer = max(differ, maxDiffer)
     else:
+        # 마지막 거리 포함
         total += nonSkip[1]
 
     print(total - maxDiffer)    
