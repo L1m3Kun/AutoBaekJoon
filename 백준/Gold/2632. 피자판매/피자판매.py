c867d075dbb2ef8daee5c2723346bd5304cdc340
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-05-14 17:18:10 +0900

    [Gold II] Title: 피자판매, Time: 664 ms, Memory: 118772 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.py
+++ 백준/Gold/2632. 피자판매/피자판매.py
@@ -1,62 +1,32 @@
-# 2632 피자판매
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
+want = int(input())
+A, B = map(int, input().split())
+pizzaA = [int(input()) for _ in range(A)]
+pizzaB = [int(input()) for _ in range(B)]
 
-def main():
-    needs = int(input())
-    m, n = map(int, input().split())
-    ans = 0
-    A, B = [0]*m, [0]*n
-    for i in range(m):
-        A[i] = int(input())
+def pizzaPcs(pizza):
+    size = len(pizza)
+    pizza = pizza + pizza
+    sizeDict = {0: 1}
+    fin_num = 0
+    for start in range(size):
+        pcs = 0
+        for end in range(start, start+size-1):
+            pcs += pizza[end]
+            if pcs > want:
+                break
+            sizeDict[pcs] = sizeDict.get(pcs, 0) + 1
     
-    for i in range(n):
-        B[i] = int(input())
+    sizeDict[sum(pizza)//2] = 1
+    return sizeDict
 
-    candidate = {
-        "A": defaultdict(int),
-        "B": defaultdict(int),
-    }
+ADict = pizzaPcs(pizzaA)
+BDict = pizzaPcs(pizzaB)
+ans = 0
+for key, value in ADict.items():
+    if want - key in BDict:
+        ans += BDict[want - key] * ADict[key]
 
-    def makeCandidate(cand_name:str, array:list, length:int):
-        cnt = 0
-        fin_num = 0
-        for i in range(length):
-            pieces = 0
-            for j in range(i, length+i):
-                if j == length+i-1:
-                    fin_num = pieces+array[j%length]
-                    continue
-                pieces += array[j%length]
-                if pieces < needs:
-                    candidate[cand_name][pieces] += 1
-                elif pieces == needs:
-                    cnt += 1
-                    break
-                else:
-                    break
-        if fin_num < needs:
-            candidate[cand_name][fin_num] += 1
-        elif fin_num == needs:
-            cnt += 1
-
-        return cnt
-
-    ans += makeCandidate("A", A, m)
-    ans += makeCandidate("B", B, n)
-
-    # print(candidate, ans)
-
-    
-    for c in candidate["A"]:
-        ans += candidate["B"][needs-c] * candidate["A"][c]
-        
-    print(ans)
-
-    return
-
-
-if __name__ == "__main__":
-    main()
+print(ans)
