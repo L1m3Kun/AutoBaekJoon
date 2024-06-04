32fe67ff07600b5b0c0b9352b1ff0d0872661967
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-06-04 11:27:33 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ Python3/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -11,18 +11,13 @@ def solution(edges):
         while stack:
             spot = stack.pop()
             
-            if cnt and spot == start:
-                
-                return 1
-                
             if len(graph[spot]) > 1:
                 return 3
             
             for node in graph[spot]:
-                if node in v:
-                    cnt[node] += 1
-                    if cnt[node] > 2:
-                        mode += 1
+                if node == start:
+                    return 1
+    
                 v.add(node)
                 stack.append(node)
         
@@ -47,12 +42,11 @@ def solution(edges):
     for n in addi_node:
         if len(graph[n]) > 1:
             target = n
-    # print(target)
+    
     answer[0] = target
-    # print(graph)
+    
     
     for start in graph[target]:
-        # print(start)
         answer[dfs(start)] += 1
         
     
