c47b772e8abac86a4dddaf8d1caf3db6d2b02f1d
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-07-03 17:48:47 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- node.js/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ node.js/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,9 +1,8 @@
 const fs =require('fs');
 const N = Number(fs.readFileSync('/dev/stdin').toString().trim());
-const ans = [];
+let ans = '';
 
 for (let i=0;i < parseInt(N/4); i++){
-    ans.push('long');
+    ans += 'long ';
 }
-ans.push('int');
-console.log(ans.join(' '));
+console.log(ans+'int');
