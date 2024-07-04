9113fa098d990a6a300b64b0d41d03f8cb21cea4
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-07-04 23:08:48 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let answer = '';
+input.slice(1).forEach((e,index) => {
+    const [a, b] = e.split(' ').map(Number);
+    answer+=`Case #${index+1}: ${a+b}\n`;
+});
+console.log(answer);
