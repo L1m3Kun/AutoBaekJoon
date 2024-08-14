60cc4f02fb9ef3f47499beab055e2ca98ddcbf25
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-08-14 11:47:13 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 160 ms, Memory: 11916 KB -BaekjoonHub

---
+++ node.js/백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin')
+                .toString()
+                .trim()
+                .split('\n')
+                .map(e => e.split(' ').map(Number));
+const [N, M] = input[0];
+const friends = {};
+input.slice(1).forEach(([l, r]) => {
+    if (l in friends){
+        friends[l].add(r);
+    } else {
+        friends[l] = new Set([r]);
+    }
+    
+    if (r in friends) {
+        friends[r].add(l);
+    } else {
+        friends[r] = new Set([l]);
+    }
+});
+
+const ans = Array(N).fill(0);
+
+for (let s = 1; s<=N;s++){
+    const stack = [s];
+    const visited = Array(N).fill(-1);
+    visited[s-1] = 0;
+    while (stack.length > 0){
+        const node = stack.shift();
+        for (edge of [...friends[node]]) {
+            if (visited[edge-1] === -1){
+                visited[edge-1]= visited[node-1] + 1;
+                stack.push(edge);
+            }
+        }
+    }
+    ans[s-1] = visited.reduce((prev, now) => now === -1? prev:prev+now,0);
+    
+}
+let minIdx = 0;
+ans.forEach((e,i) => {
+    if ( e < ans[minIdx]) minIdx = i;
+});
+console.log(minIdx+1);
