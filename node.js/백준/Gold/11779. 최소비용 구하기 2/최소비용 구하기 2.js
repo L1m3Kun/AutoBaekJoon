cf3a007a77e4b856e857f80bc807e83aa7aa06d5
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-09-06 11:08:45 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 520 ms, Memory: 86660 KB -BaekjoonHub

---
+++ node.js/백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+const busLines = Array.from(Array(N + 1), () => []);
+
+input.slice(2, 2 + M).forEach((el) => {
+  const [s, e, c] = el.split(' ').map(Number);
+  busLines[s].push([e, c]);
+});
+
+const [start, end] = input[M + 2].split(' ').map(Number);
+
+const heapQ = [0];
+
+const pushQ = (item) => {
+  heapQ.push(item);
+  let head = heapQ.length - 1;
+  while (head >> 1 > 0) {
+    if (heapQ[head >> 1][1] > heapQ[head][1]) {
+      [heapQ[head >> 1], heapQ[head]] = [heapQ[head], heapQ[head >> 1]];
+    }
+    head >>= 1;
+  }
+};
+
+const popQ = () => {
+  const popItem = heapQ[1];
+  if (heapQ.length === 2) {
+    return heapQ.pop();
+  }
+
+  heapQ[1] = heapQ.pop();
+  let head = 1;
+  while (head < heapQ.length) {
+    let node = head;
+    const [left, right] = [node << 1, (node << 1) + 1];
+
+    if (right < heapQ.length && heapQ[right][1] < heapQ[node][1]) {
+      node = right;
+    }
+    if (left < heapQ.length && heapQ[left][1] < heapQ[node][1]) {
+      node = left;
+    }
+    if (node === head) break;
+    [heapQ[head], heapQ[node]] = [heapQ[node], heapQ[head]];
+    head = node;
+  }
+  return popItem;
+};
+
+
+
+const cities = Array(N+1).fill(100000001);
+cities[start] = 1;
+pushQ([start, 0, [start]]);
+let ans = [];
+while (heapQ.length > 0) {
+  const [edge, cost, v] = popQ();
+
+  if (edge === end) {
+    ans.push(cost);
+    ans.push(v.length);
+    ans.push(v.join(' '));
+    break;
+  }
+  for (let [node, c] of busLines[edge]) {
+    if (cities[node] > cost+c){
+      cities[node] = cost+c;
+      pushQ([node, cost + c, [...v, node]]);    
+    }
+  }
+}
+
+console.log(ans.join('\n'));
