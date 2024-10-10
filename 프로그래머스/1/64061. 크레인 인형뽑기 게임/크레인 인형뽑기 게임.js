60accd9a264d12b1312f05b84142358ff713f60c
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-10-10 17:58:27 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.82 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,42 +1,21 @@
 function solution(board, moves) {
-    var answer = 0;
-    const stack = [];
-    const height = board.length, width = board[0].length;
-    
-    const setIdxArray = (w, h) => {
-        const idxArray = Array(w).fill(h-1);
-        for (let i = 0; i < w; i++) {
-            for (let j = 0; j < h; j++) {
-                if (board[j][i]) {
-                    idxArray[i] = j;
-                    break;
-                }
-            }
-        }
-        return idxArray;
-    }
-    
-    const dollIdxArray = setIdxArray(width, height);
-    // console.log(dollIdxArray)
-    
-    moves.forEach(e => {
-        // console.log('dollidx', dollIdxArray);
-        const w = e-1;
-        const h = dollIdxArray[w];
-        if (h < height) {
-            const doll = board[h][w];
-            if (stack.length > 0 && doll === stack[stack.length-1]){
-                stack.pop();
-                answer ++;
+    const columns = board[0].map((_, colIndex) => board.map(row => row[colIndex]).reverse())
+    const filteredColumns = columns.map(column => column.filter(doll => doll !== 0))
+    const dollBucket = moves.map((command)=> filteredColumns[command-1].pop()).filter(item => item !== undefined)
+    const [_,score] = dollBucket.reduce(
+        ([crushedDolls,score], char)=> {
+            if(crushedDolls.length > 0 && crushedDolls[crushedDolls.length -1] === char){
+                crushedDolls.pop();
+                score += 2;
             } else {
-                stack.push(doll);
+                crushedDolls.push(char)
             }
-            dollIdxArray[w] ++;    
-        } 
-        // console.log('e', e);
-        // console.log('stack', stack);
-        // console.log('answer', answer);
-        
-    });
-    return answer * 2;
+            return [crushedDolls, score]
+        },
+        [[], 0]
+    )
+    
+  
+    return score
+    
 }
