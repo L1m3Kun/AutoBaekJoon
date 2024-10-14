94fdbe016b99aa94f9698805c18e9f512c152a10
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-10-14 14:36:45 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.71 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 0.82 ms
+메모리: 33.9 MB, 시간: 0.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 10일 17:58:25
+2024년 10월 14일 14:36:42
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,21 +1,51 @@
+const setIdxArray = (w, h, board) => {
+        const idxArray = Array(w).fill(h-1);
+        for (let i = 0; i < w; i++) {
+            for (let j = 0; j < h; j++) {
+                if (board[j][i]) {
+                    idxArray[i] = j;
+                    break;
+                }
+            }
+        }
+        return idxArray;
+    }
+
 function solution(board, moves) {
-    const columns = board[0].map((_, colIndex) => board.map(row => row[colIndex]).reverse())
-    const filteredColumns = columns.map(column => column.filter(doll => doll !== 0))
-    const dollBucket = moves.map((command)=> filteredColumns[command-1].pop()).filter(item => item !== undefined)
-    const [_,score] = dollBucket.reduce(
-        ([crushedDolls,score], char)=> {
-            if(crushedDolls.length > 0 && crushedDolls[crushedDolls.length -1] === char){
-                crushedDolls.pop();
+    
+    const stack = [];
+    const height = board.length, width = board[0].length;
+    
+    const dollIdxArray = setIdxArray(width, height, board);
+    
+    // moves.forEach(e => {
+    //     const w = e-1;
+    //     const h = dollIdxArray[w];
+    //     if (h < height) {
+    //         const doll = board[h][w];
+    //         if (stack.length > 0 && doll === stack[stack.length-1]){
+    //             stack.pop();
+    //             answer ++;
+    //         } else {
+    //             stack.push(doll);
+    //         }
+    //         dollIdxArray[w] ++;    
+    //     } 
+    // });
+    const [_, answer] = moves.reduce(([stack, score], move) => {
+        const w = move - 1;
+        const h = dollIdxArray[w];
+        if (h < height) {
+            const doll = board[h][w];   // 인형 위치로 인형 값 가져옴
+            if (stack.length > 0&& doll === stack[stack.length -1]) {
+                stack.pop();
                 score += 2;
             } else {
-                crushedDolls.push(char)
+                stack.push(doll);
             }
-            return [crushedDolls, score]
-        },
-        [[], 0]
-    )
-    
-  
-    return score
-    
+            dollIdxArray[w] ++; // 최상위 인덱스를 증가하므로써 현재 최상위 인형을 제외하고 실행
+        }
+        return [stack, score]
+    }, [[], 0]);
+    return answer;
 }
