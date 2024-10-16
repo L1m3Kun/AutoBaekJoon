c1bdf85c40f699047498f825a76a59b975ccf569
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-10-16 15:38:13 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -12,40 +12,25 @@ const setIdxArray = (w, h, board) => {
     }
 
 function solution(board, moves) {
-    
+    let answer = 0;
     const stack = [];
     const height = board.length, width = board[0].length;
     
     const dollIdxArray = setIdxArray(width, height, board);
     
-    // moves.forEach(e => {
-    //     const w = e-1;
-    //     const h = dollIdxArray[w];
-    //     if (h < height) {
-    //         const doll = board[h][w];
-    //         if (stack.length > 0 && doll === stack[stack.length-1]){
-    //             stack.pop();
-    //             answer ++;
-    //         } else {
-    //             stack.push(doll);
-    //         }
-    //         dollIdxArray[w] ++;    
-    //     } 
-    // });
-    const [_, answer] = moves.reduce(([stack, score], move) => {
-        const w = move - 1;
+    moves.forEach(e => {
+        const w = e-1;
         const h = dollIdxArray[w];
         if (h < height) {
-            const doll = board[h][w];   // 인형 위치로 인형 값 가져옴
-            if (stack.length > 0&& doll === stack[stack.length -1]) {
+            const doll = board[h][w];
+            if (stack.length > 0 && doll === stack[stack.length-1]){
                 stack.pop();
-                score += 2;
+                answer ++;
             } else {
                 stack.push(doll);
             }
-            dollIdxArray[w] ++; // 최상위 인덱스를 증가하므로써 현재 최상위 인형을 제외하고 실행
-        }
-        return [stack, score]
-    }, [[], 0]);
-    return answer;
+            dollIdxArray[w] ++;    
+        } 
+    });
+    return answer << 1;
 }
