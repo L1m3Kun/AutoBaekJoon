ed0b557ba00b4fddf35f5950387f8689bf2310ae
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-11-25 15:46:06 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,1 +1,6 @@
-const solution = (rsp) => Array.from(rsp).reduce((wins, enermy) => enermy === '2'? wins+'0' : enermy === '0' ? wins+'5' : wins+'2', '');
+const win_rules = {
+    2: '0',
+    0: '5',
+    5: '2'
+};
+const solution = (rsp) => Array.from(rsp).reduce((wins, enermy) => wins+ win_rules[enermy], '');
