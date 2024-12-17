64c6234510f2d3980c35951126b38e9d044a322b
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-12-17 22:07:55 +0900

    [level 0] Title: 카운트 다운, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ JavaScript/프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,4 +1,12 @@
+// 요구사항: start_num에서 end_num까지 배열에 1씩 감소하여 담는다.
+// function solution(start_num, end_num) {
+//     const answer = [];
+//     for(let num = start_num; num >= end_num; num-=1){
+//         answer.push(num);
+//     }
+//     return answer;
+// }
+
 function solution(start_num, end_num) {
-    
-    return Array.from({length: start_num-end_num + 1}, (_,i)=>start_num-i);
+    return Array.from({length: start_num - end_num +1}, (_,i) => start_num-i);
 }
