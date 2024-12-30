f6ea631f805b63e0b9446005d41a101b7661d52a
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-12-30 15:16:17 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,8 +1,13 @@
+// function solution(numbers, n) {
+//     let answer = 0;
+//     for (num of numbers){
+//         if (answer > n) break;
+//         answer += num;
+//     }
+//     return answer
+// }
+
 function solution(numbers, n) {
-    let answer = 0;
-    for (num of numbers){
-        if (answer > n) break;
-        answer += num;
-    }
-    return answer
+    const sum = numbers.reduce((prev, num) => (prev <= n ? prev+num : prev), 0)
+    return sum
 }
