738d8687ccb429ebe3ba2bb338e51ab701963f07
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-05-21 01:29:10 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 188 ms, Memory: 18988 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
@@ -4,19 +4,16 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const [N, M] = input[0].split(" ").map(Number);
 const matrix = input.slice(1, N+1).map((e) => e.split(" ").map(Number));
 const testCase = input.slice(N+2, -1).map((e)=> e.split(" ").map(Number));
+const prefix = Array.from({length: N+1}, () => Array(M+1).fill(0));
 const answer = [];
 
-const solution = (matrix, i, j, x, y) => {
-    let answer = 0;
-    for(let r = i; r < x; r++){
-        for (let c = j; c < y ; c++) {
-            answer += matrix[r][c];
-        }
+for(let r = 1; r <= N; r++){
+    for (let c = 1; c <= M; c++){
+        prefix[r][c] = prefix[r][c-1] + prefix[r-1][c] - prefix[r-1][c-1] + matrix[r-1][c-1];
     }
-    return answer;
 }
 
 testCase.forEach(([i, j, x, y]) => {
-    answer.push(solution(matrix, i-1, j-1, x, y));
+    answer.push(prefix[x][y] - prefix[x][j-1] - prefix[i-1][y] + prefix[i-1][j-1]);
 });
 console.log(answer.join("\n"));
