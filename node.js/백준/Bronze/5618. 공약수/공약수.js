d566c3d80d246c76d4b325689ca66377304ba0a1
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-05-29 21:13:39 +0900

    [Bronze II] Title: 공약수, Time: 92 ms, Memory: 9616 KB -BaekjoonHub

--- node.js/백준/Bronze/5618. 공약수/공약수.js
+++ node.js/백준/Bronze/5618. 공약수/공약수.js
@@ -2,10 +2,25 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input[0]);
 const numbers = input[1].split(' ').map(Number);
-const ans = [];
-for (let i = 1;i<=Math.min(...numbers);i++){
-  if(numbers.every((num)=> !(num % i))){
-    ans.push(i);
-  }
+
+const gcd = (a, b) => {
+    while (b !== 0) {
+        const tmp = a%b;
+        a = b
+        b = tmp;
+    }
+    return a;
 }
+const res = n > 2? gcd(gcd(numbers[0], numbers[1]), numbers[2]):gcd(numbers[0], numbers[1]);
+
+const ans = [];
+for (let i = 1 ; i <= Math.sqrt(res); i++){
+    if (!(res % i)) {
+        if (i !== res/i){
+            ans.push(res/i);
+        }
+        ans.push(i);
+    }
+}
+ans.sort((a,b) => a-b);
 console.log(ans.join('\n'));
