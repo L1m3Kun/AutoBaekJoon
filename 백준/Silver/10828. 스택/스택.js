e851a862579012e0df71e6b0d6ced68ca9697a9b
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-06-20 12:50:36 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const answer = input.slice(1).reduce(([ans, stack], condition) => {
+    const [order, element] = condition.split(' ');
+    switch(order) {
+        case "push":
+            stack.push(Number(element));
+            break;
+        case "pop":
+            ans.push(stack.pop() || -1);
+            break;
+        case "size":
+            ans.push(stack.length);
+            break;
+        case "empty":
+            ans.push(!stack.length ? 1:0);
+            break;
+        case "top":
+            ans.push(stack[stack.length-1] || -1);
+            break;
+        default:
+            console.log("error defaults");
+            return ;
+    }
+    return [ans, stack];
+}, [[], []]);
+
+console.log(answer[0].join('\n'));
