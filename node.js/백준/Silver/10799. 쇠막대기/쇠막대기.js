837287e98778ca7e21d23a81a45189b070088055
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-06-23 00:20:21 +0900

    [Silver II] Title: 쇠막대기, Time: 196 ms, Memory: 15000 KB -BaekjoonHub

--- node.js/백준/Silver/10799. 쇠막대기/쇠막대기.js
+++ node.js/백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
 
-const [_, answer] = Array.from(input).reduce(([stack, ans, isLaser], str) => {
-
+const [_, answer] = Array.from(input).reduce(([stack, ans], str, idx) => {
     if (str === "("){
         stack ++;
-        isLaser = 1;
     } else {
-        if (isLaser) ans += stack - 1;
-        else ans ++;
         stack --;
-        isLaser = 0;
+        if (input[idx-1]==="(") ans += stack;
+        else ans ++;
     }
-    return [stack, ans, isLaser];
-}, [0,0, 0]);
+    return [stack, ans];
+}, [0,0]);
 console.log(answer);
