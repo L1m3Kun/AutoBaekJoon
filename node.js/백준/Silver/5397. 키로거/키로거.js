1fe61178ee5d9b129fec4de04c7783cf962912ce
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-07-15 23:45:46 +0900

    [Silver II] Title: 키로거, Time: 560 ms, Memory: 148980 KB -BaekjoonHub

--- node.js/백준/Silver/5397. 키로거/키로거.js
+++ node.js/백준/Silver/5397. 키로거/키로거.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
 const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const ans = inputs.slice(1).map((password)=>{
-    const [prefix, postfix] = Array.from(password).reduce(([prefix, postfix], char)=> {
-            switch(char) {
-                case "<":
-                    if (prefix.length < 1) break;
-                    postfix.push(prefix.pop());
-                    break;
-                case ">":
-                    if (postfix.length < 1) break;
-                    prefix.push(postfix.pop());
-                    break;
-                case "-":
-                    if (prefix.length < 1) break;
-                    prefix.pop();
-                    break;
-                default:
-                    prefix.push(char);
-                    break;
-            }
-            return [prefix, postfix];
-        },[[], []]);
+    const prefix = [];
+    const postfix = [];
+    for (const char of password) {
+        switch(char) {
+            case "<":
+                if (prefix.length < 1) break;
+                postfix.push(prefix.pop());
+                break;
+            case ">":
+                if (postfix.length < 1) break;
+                prefix.push(postfix.pop());
+                break;
+            case "-":
+                if (prefix.length < 1) break;
+                prefix.pop();
+                break;
+            default:
+                prefix.push(char);
+                break;
+        }
+    }
 
     return prefix.join('') + postfix.reverse().join('');
 });
