6094ae7e746b11bd4867dbed84c02b801135f66d
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-11-17 20:23:33 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

--- node.js/백준/Gold/1759. 암호 만들기/암호 만들기.js
+++ node.js/백준/Gold/1759. 암호 만들기/암호 만들기.js
@@ -6,21 +6,16 @@ const inputs = fs.readFileSync('/dev/stdin')
 const [L, C] = inputs[0].split(" ").map(Number);
 const arr = inputs[1].split(" ").sort();
 const moem = new Set(['a', 'e', 'i', 'o', 'u']);
+const lastIndexOfMoem = arr.reduce((i ,a, idx)=>moem.has(a) ? idx : i, 0);
 const ans = [];
 const dfs = (idx, secret, hasMoem) => {
-
-    if (secret.length === L) {
-
-        return hasMoem && (L-hasMoem) >= 2 && ans.push(secret);
+    if (secret.length === L) return hasMoem && (L-hasMoem) > 1 && ans.push(secret);
+    if (idx === C || (C - idx + secret.length) < L) return;
+    if (moem.has(arr[idx])) dfs(idx+1, secret + arr[idx], hasMoem+1); 
+    else {
+        if (!hasMoem && lastIndexOfMoem < idx) return;
+        dfs(idx+1, secret + arr[idx], hasMoem); 
     }
-    if (idx === C) return;
-
-    if(moem.has(arr[idx])){
-        dfs(idx+1, secret + arr[idx], hasMoem+1);             
-    } else {
-        dfs(idx+1, secret + arr[idx], hasMoem);             
-    }
-
     dfs(idx + 1, secret, hasMoem);
 }
 
