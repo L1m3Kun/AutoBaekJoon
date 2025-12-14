dd1e0a892ad3019093ce5dcc7d5f95a6be9e2318
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-12-14 14:59:28 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 436 ms, Memory: 12168 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const [[N, K], A] = fs.readFileSync("/dev/stdin")
+                      .toString()
+                      .trim()
+                      .split("\n")
+                      .map((line)=>line.split(" ").map(Number));
+
+const bubbleSort = (arr) => {
+    let k = 0;
+    for (let last = N-1; last > 0; last --){
+        for (let i = 0; i < last; i++){
+            if (arr[i] > arr[i+1]) {
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                k ++;
+                if (k === K) return [arr[i], arr[i+1]].join(" ");
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(bubbleSort(A));
