80537b9d6aa0ca758868e2eb3073fff7303875e2
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-12-14 14:49:08 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 192 ms, Memory: 10800 KB -BaekjoonHub

--- node.js/백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.js
+++ node.js/백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.js
@@ -6,7 +6,10 @@ const A = inputs[1].split(" ").map(Number);
 const selectionSort = (arr) => {
     let k = 0;
     for (let last = N; last > 0 ; last--) {
-        const maxIdx = arr.slice(0, last).reduce((res, num, i, origin)=>origin[res] < num ? i : res , 0);
+        let maxIdx=last-1;
+        for (let i = last-1; i >=0; i--){
+            if (arr[i] > arr[maxIdx]) maxIdx = i;
+        }
         if (maxIdx !== last - 1) {
             [arr[maxIdx], arr[last-1]] = [arr[last-1], arr[maxIdx]];
             k ++;
