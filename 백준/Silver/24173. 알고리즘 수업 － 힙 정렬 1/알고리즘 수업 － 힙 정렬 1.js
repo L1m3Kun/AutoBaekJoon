d82cf74c109a7154a1ae007b2003f7add268b778
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2025-12-20 22:47:43 +0900

    [Silver IV] Title: 알고리즘 수업 - 힙 정렬 1, Time: 504 ms, Memory: 68624 KB -BaekjoonHub

---
+++ 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.js
@@ -0,0 +1,63 @@
+const [[N, K], A] = require("fs").readFileSync("/dev/stdin")
+                                 .toString()
+                                 .trim()
+                                 .split("\n")
+                                 .map((line) => line.split(" ").map(Number));
+
+let k = 0;
+const changed = [0, 0];
+const heapify = (A, node, n) => {
+    let [left, right] = [(node << 1) + 1, (node<<1) + 2];
+    //console.log("node, left, right: ", node, left, right);
+    let smaller = -1;
+    if (right <= n) {
+        if (A[left] <= A[right]) smaller = left;
+        else smaller = right;
+    } else if (left <= n) {
+        smaller = left;
+    } else return;
+    //console.log("    smaller: ", smaller);
+    if (A[smaller] < A[node]) {
+        [A[node], A[smaller]] = [A[smaller], A[node]];
+        //console.log("    changed: ", smaller, node, A)
+        //console.log("    k: ", k)
+        if (++k===K) {
+            changed[0] = A[node];
+            changed[1] = A[smaller];
+            return -1;
+        }        
+        heapify(A, smaller, n);
+
+    }
+}
+  
+const buildMinHeap = (A, n) => {
+  //console.log("==build==")
+  for (let i = (n >> 1)+1; i >= 0; i--){
+      const flag = heapify(A, i, n);
+      if (flag < 0) return -1;
+  }
+}
+
+
+
+const heapSort = (A) => {
+  const flag = buildMinHeap(A, N-1);
+  if (flag < 0) return;
+  //console.log("==sort==")
+  for (let i = N-1; i > 0; i--) {
+      [A[0], A[i]] = [A[i], A[0]];
+      //console.log("    changed: ", 0, i, A);
+      //console.log("    k: ", k)
+      if (++k===K) {
+          changed[0] = A[i];
+          changed[1] = A[0];
+          return ;
+      }
+      const flag = heapify(A, 0, i-1);
+      if (flag < 0) return;
+  }  
+};
+
+heapSort(A);
+console.log(k>=K ? changed.join(" ") : -1);
