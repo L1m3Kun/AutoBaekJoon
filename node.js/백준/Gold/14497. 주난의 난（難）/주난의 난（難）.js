ace3a6b603cfeb53e0b8d056b8c0fc059d72b87f
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2026-03-05 17:39:48 +0900

    [Gold IV] Title: 주난의 난(難), Time: 200 ms, Memory: 16288 KB -BaekjoonHub

--- node.js/백준/Gold/14497. 주난의 난（難）/주난의 난（難）.js
+++ node.js/백준/Gold/14497. 주난의 난（難）/주난의 난（難）.js
@@ -2,18 +2,12 @@ const fs = require("fs");
 const inputs = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [N, M] = inputs[0].split(" ").map(Number);
 const [x1, y1, x2, y2] = inputs[1].split(" ").map(Number).map((num) => num-1);
-const classroom = inputs.slice(2)
-                        .map((l) => l.split("")
-                                     .map((item) => {
-                            if (item === "*" || item === "#") return item;
-                            return +item;
-                        }));
+const classroom = inputs.slice(2);
 const directions = [[1,0], [0,1], [-1,0], [0,-1]];
 const field = Array.from({length: N}, () => Array(M).fill(-1));
 (() => {
   const queue = [[x1,y1,0]];
   let head = 0;
-  const visited = new Set([x1 * M + y1]);
   field[x1][y1] = 0;
   while(queue.length > head) {
     const [x, y, count] = queue[head];
@@ -26,11 +20,10 @@ const field = Array.from({length: N}, () => Array(M).fill(-1));
         ny < 0 ||
         ny >= M
       ) continue;
-      if (visited.has(nx * M + ny)) continue;
-      visited.add(nx * M + ny);
+      if (field[nx][ny] >= 0) continue;
 
       field[nx][ny] = count;
-      if (classroom[nx][ny] === 1 ) {
+      if (classroom[nx][ny] === "1" ) {
         queue.push([nx,ny, count+1]);
         continue;
       }
