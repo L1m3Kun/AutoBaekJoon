35e8946636d7b362dcdfb90b9efe5e6cea1a3bdd
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2026-03-10 20:44:04 +0900

    [Gold IV] Title: 암벽 등반, Time: 560 ms, Memory: 39500 KB -BaekjoonHub

--- node.js/백준/Gold/2412. 암벽 등반/암벽 등반.js
+++ node.js/백준/Gold/2412. 암벽 등반/암벽 등반.js
@@ -11,37 +11,31 @@ const sortedByY = halls.reduce((wall, [x,y], idx) => {
     maxX = Math.max(maxX, x);
     return wall;
 }, Array.from({length: T+1}, () => []))
-let minV = INF;
 
-(() => {
+const ans = (() => {
   const queue = [[0,0,0]];
   let head = 0;
   const visited = new Set([]);
   while (queue.length > head) {
-//  console.log(`===${head}===`)
     const [x, y, count] = queue[head];
     queue[head++] = null;
-//  console.log(`x= ${x}, y= ${y}`);
 
     if (y === T) {
-      minV = count;
-      break;
+      return count;
     }
-//  console.log("---for---")
-      
     for (let range = 2; range >= -2; range --) {
-        const dy = y+range;
-        if (dy < 0 || dy > T) continue;
-        for (const dx of sortedByY[dy]) {
-            //console.log(dy, dx, dy*maxX+dx)
-            if (visited.has(dy*maxX+dx)) continue;
-            if (Math.abs(dx-x) > 2) continue;
-            queue.push([dx, dy, count+1]);
-            visited.add(dy*maxX+dx);
-        }
+      const dy = y+range;
+      if (dy < 0 || dy > T) continue;
+      for (const dx of sortedByY[dy]) {
 
+        if (Math.abs(dx-x) > 2) continue;
+        if (visited.has(dy*maxX+dx)) continue;
+          queue.push([dx, dy, count+1]);
+          visited.add(dy*maxX+dx);
+      }
     }
   }
+  return -1;
 })();
 
-console.log(minV === INF ? -1 : minV)
+console.log(ans);
