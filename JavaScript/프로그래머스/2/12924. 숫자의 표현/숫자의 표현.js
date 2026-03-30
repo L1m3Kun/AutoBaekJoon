76e64d57b0a85d597f3ef5062d99d51c8bb59c5d
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2026-03-30 15:00:40 +0900

    [level 2] Title: 숫자의 표현, Time: 0.34 ms, Memory: 33 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,16 +1,9 @@
 function solution(n) {
     let answer = 0;
-    let [i,j] = [0,1];
-    let sum = 1;
-    while (i < j) {
-        if (sum >= n) {
-            if (sum === n) answer ++;
-            i ++;
-            sum -= i;
-        } else {
-            j ++;
-            sum += j;
-        } 
+    for (let i = 1; i<=n; i++) {
+        if ((n % i === 0) && (i & 1)){
+            answer ++;
+        }
     }
     return answer;
 }
